cb691470e81d36c7b631637e91ffaeb238ae0648
javyer <70788090+javyer12@users.noreply.github.com>
2022-10-03 23:38:08 -0600

    header finished

---
+++ next.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  reactStrictMode: true,
+}
+
+// env: {
+//   customKey: "customValue",
+// },
+// basePath: "/dist",
+// compress: true,
+// async redirect() {
+//   return [
+//     {
+//       source: "/clicl",
+//       destination: "/urlToSend",
+//       permanent: true,
+//     }
+//   ]
+// }
